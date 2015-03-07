18c94e4158acc1edbb6b4809aae20066a5036ae6
zack-bitcoin <zack.bitcoin@gmail.com>
2015-03-07 16:06:16 +0000

    fixed bug where marketcap and size never increased

---
+++ README.md
@@ -0,0 +1,5 @@
+mini-DAO
+
+This is a tiny governance contract. It was designed to maintain the list of validators for tendermint.
+
+try this: ./run.py validators
--- validators_test.py
+++ validators_test.py
@@ -18,12 +18,12 @@ def mk_acc(n):
 def mk_sig(hash, priv): return list(b.ecdsa_raw_sign(hash, priv))
 def mk_mk_sig(msghash): return (lambda p: mk_sig(msghash, p["priv"]))
 def add(a, b): return a+b
-def add_validator(location, addr, balance, accs, c, nonce):
+def add_validator(location, addr, balance, accs, new_market_cap, new_size, c, nonce):
     f=mk_mk_sig(hash_list([addr, location, balance, nonce, 1337]))
-    c.update(addr, location, balance, reduce(add, map(f, accs)))
+    c.update(addr, location, balance, new_market_cap, new_size, reduce(add, map(f, accs)))
 def test(c):
     accs=map(mk_acc, [1,2,3])
     balance = 10000000
-    add_validator(1, accs[1]["addr"], balance, accs[0:1], c, 0)
-    add_validator(2, accs[2]["addr"], balance, accs[0:2], c, 1)
+    add_validator(1, accs[1]["addr"], balance, accs[0:1], 2*balance, 2, c, 0)
+    add_validator(2, accs[2]["addr"], balance, accs[0:2], 3*balance, 3, c, 1)
 
